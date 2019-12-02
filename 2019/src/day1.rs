da2b872bdb9b460c1360bd4af3d87a473fed2233
Yang Su <yang@quip.com>
2019-12-02 18:23:45 +0100

    Fix ref weirdness in day1

--- 2019/src/day1.rs
+++ 2019/src/day1.rs
@@ -4,14 +4,13 @@ use std::path::Path;
 
 pub fn day1(filename: impl AsRef<Path>) -> (i32, i32) {
     let lines = read_ints(filename);
-    return (
-        calc_fuel_for_lines(&lines, calc_fuel),
-        calc_fuel_for_lines(&lines, calc_total_fuel),
-    );
-}
-
-fn calc_fuel_for_lines(lines: &Vec<i32>, calc: fn(&i32) -> i32) -> i32 {
-    return lines.into_iter().map(calc).sum();
+    let mut fuel = 0;
+    let mut total_fuel = 0;
+    for line in lines {
+        fuel += calc_fuel(line);
+        total_fuel += calc_total_fuel(line);
+    }
+    return (fuel, total_fuel);
 }
 
 fn read_ints(filename: impl AsRef<Path>) -> Vec<i32> {
@@ -25,28 +24,28 @@ fn read_ints(filename: impl AsRef<Path>) -> Vec<i32> {
 
 #[test]
 fn test_calc_fuel() {
-    assert_eq!(calc_fuel(&12), 2);
-    assert_eq!(calc_fuel(&14), 2);
-    assert_eq!(calc_fuel(&1969), 654);
-    assert_eq!(calc_fuel(&100756), 33583);
+    assert_eq!(calc_fuel(12), 2);
+    assert_eq!(calc_fuel(14), 2);
+    assert_eq!(calc_fuel(1969), 654);
+    assert_eq!(calc_fuel(100756), 33583);
 }
 
-fn calc_fuel(weight: &i32) -> i32 {
-    return (*weight / 3 - 2).max(0);
+fn calc_fuel(weight: i32) -> i32 {
+    return (weight / 3 - 2).max(0);
 }
 
 #[test]
 fn test_calc_total_fuel() {
-    assert_eq!(calc_total_fuel(&14), 2);
-    assert_eq!(calc_total_fuel(&1969), 966);
-    assert_eq!(calc_total_fuel(&100756), 50346);
+    assert_eq!(calc_total_fuel(14), 2);
+    assert_eq!(calc_total_fuel(1969), 966);
+    assert_eq!(calc_total_fuel(100756), 50346);
 }
 
-fn calc_total_fuel(weight: &i32) -> i32 {
+fn calc_total_fuel(weight: i32) -> i32 {
     let mut total_fuel = 0;
-    let mut current_fuel = *weight;
+    let mut current_fuel = weight;
     while current_fuel > 0 {
-        current_fuel = calc_fuel(&current_fuel);
+        current_fuel = calc_fuel(current_fuel);
         total_fuel += current_fuel;
     }
     return total_fuel;
